ffcf1ed6c5216b5e4d0e5896a0e8294a7badda2d
Javier Romero <romeropaolini@gmail.com>
2019-02-03 19:26:26 +0100

    Set up react router!

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,5 +4,17 @@ import css from './styles/style.styl'
 
 // Import components
 import Main from './components/Main'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-render(<Main />, document.getElementById('root'))
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/postId' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
